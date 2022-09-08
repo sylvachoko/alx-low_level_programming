f9e3feccdd58568966babf6f57abd16d02756be7
Sylva <onyekanwachokor@gmail.com>
2022-09-08 07:48:54 -0700

    descriptions

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - start
+ *
+ * Description: Print some words
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+/**
+ * main - print various types and their sizes
+ * Description - to print the sizes of various datatypes
+ * Return: 0
+ */
+
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
-printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-return (0);
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return (0);
 }
 
