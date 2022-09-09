1c7da730a244498f326973bebb8dde90b245047f
Sylva <onyekanwachokor@gmail.com>
2022-09-09 08:02:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - puts
+ *
+ * Description: puts
+ *
+ * Return: (0)
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
