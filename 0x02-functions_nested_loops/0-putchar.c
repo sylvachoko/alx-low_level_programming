9c2838b21feafe084046138ea14bde9b89a54842
Sylva <onyekanwachokor@gmail.com>
2022-09-13 03:34:56 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -  print _putchar
+ *
+ * Description: a program that prints _putchar
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercase
+ *
+ * Description: a program that prints the alphabet in lowercase
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+char ch = 'a';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
+
