fd3e4b0164bc3566375abaa7a760e26f3a8ede4d
Sylva <onyekanwachokor@gmail.com>
2022-09-19 14:43:24 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+int l = 0;
+	char x = s[0];
+
+	while (x != '\0')
+	{
+		l++;
+		x = s[i++];
+	}
+	return (l);
+}
+
