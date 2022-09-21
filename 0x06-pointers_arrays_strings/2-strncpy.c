629033241a675bc17918a583b1b9a03deb6836ae
Sylva <onyekanwachokor@gmail.com>
2022-09-21 11:31:33 +0100

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: destination
+ * @src: source
+ * @n: number of char
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		if (i < n)
+			dest[i] = src[i];
+		while (i < n)
+			dest[i++] = '\0';
+
+	return (dest);
+}
+
