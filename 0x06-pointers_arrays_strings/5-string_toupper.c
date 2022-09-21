ea19bae8ff85bd4d9a9887032a4e40509f638cad
Sylva <onyekanwachokor@gmail.com>
2022-09-21 11:38:56 +0100

    cap

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper -  changes all lower of a string to upper
+ * @n: input string
+ * Return: uppercase string
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+	}
+	return (n);
+}
+
