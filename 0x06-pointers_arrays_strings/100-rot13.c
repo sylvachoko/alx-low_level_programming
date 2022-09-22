5044efe2010a02d8078a7ccd159a4f2662b8453a
Sylva <onyekanwachokor@gmail.com>
2022-09-22 12:20:27 +0100

    alph

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,34 +1,30 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * rot13 - Entry point
- * ONE if, TWO loops only...
- * @n: input
- * Return: decrypted string
+ * rot13 - check the code for Holberton School students.
+ *@n : variable
+ * Return: return
  */
+
 char *rot13(char *n)
 {
-	int x, rot_c = 13, i = 0;
-	char toswap[] = {'A', 'N', 'a', 'n', 'B', 'O', 'b', 'o', 'C', 'P',
-		'c', 'p', 'D', 'Q', 'd', 'q', 'E', 'R', 'e', 'r', 'F', 'S', 'f',
-		's', 'G', 'T', 'g', 't', 'H', 'U', 'h', 'u', 'I', 'V', 'i', 'v',
-		'J', 'W', 'j', 'w', 'K', 'X', 'k', 'x', 'L', 'Y', 'l', 'y', 'M',
-		'Z', 'm', 'z'};
+int i;
+int a;
+int d;
+char b[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char c[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (n[i] != '\0')
-	{
-		for (x = 0; x <= 51; x++)
-		{
-			if (n[i] == toswap[x])
-			{
-				n[i] = n[i] + rot_c;
-				x = 51;
-			}
-			rot_c = rot_c * -1;
-		}
-		i++;
-	}
-	return (n);
+for (i = 0; n[i] != '\0'; i++)
+{
+d = 1;
+for (a = 0; a < 52; a++)
+{
+if (n[i] == b[a] && d == 1)
+{
+n[i] = c[a];
+d = 0;
 }
-
+}
+}
+return (n);
+}
