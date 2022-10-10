38fd06e59d7806f8eab209df2ed9979586ef90ae
Sylva <onyekanwachokor@gmail.com>
2022-10-10 18:19:44 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - structure definition of dog
+ * @name: char string
+ * @age: int
+ * @owner: char string
+ */
+
+typedef struct dog 
+{
+    char *name;
+    float *age;
+    char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
