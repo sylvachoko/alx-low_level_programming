a3daf9f2e110e5515f482ab648638a7bde4c73c8
Sylva <onyekanwachokor@gmail.com>
2022-10-10 18:21:25 +0100

    space

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,11 @@
  * @owner: char string
  */
 
-typedef struct dog 
+typedef struct dog
 {
-    char *name;
-    float *age;
-    char *owner;
+char *name;
+float *age;
+char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
