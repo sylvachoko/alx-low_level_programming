2a13adf7a0f02f77c90a47fabf0bcff6ddef20f5
Sylva <onyekanwachokor@gmail.com>
2022-10-13 12:04:05 +0100

    Value of a variable

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	unsigned int i;
 
-	va_start (ap, n);
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, int);
+		sum += va_arg(ap, int);
 
-	va_end (ap);
-	return sum;
+	va_end(ap);
+	return (sum);
 }
 
---
+++ 0x10-variadic_functions/1-print_number.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator: separator to print between numbers
+ * @n: number of numbers to print
+ *
+ * Return: void
+ */
+
+// void print_numbers(const char *separator, const unsigned int n, ...);
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int num = 0;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (i < n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
+
