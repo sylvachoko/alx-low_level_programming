66dab6703130d70f7cfc5ccfbb3e44168c7870c7
Sylva <onyekanwachokor@gmail.com>
2022-10-20 22:35:56 +0100

    betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
-* print_list - prints all the elements of a list_t list
-* @h: pointer to the start of the list
-*
-* Return: number of nodes in the list
-*/
-
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
 size_t print_list(const list_t *h)
 {
-const list_t *lst;
-unsigned int i;
-lst = h;
-for (i = 0; lst; i++)
-{
-printf("[%u] %s\n", lst->len, lst->str);
-lst = lst->next;
-}
-return (i);
+	const list_t *tmp;
+	unsigned int i;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+	return (i);
 }
 
